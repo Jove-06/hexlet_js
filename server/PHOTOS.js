4822178ecef13889c948b45e2d100e7993b5c393
galag <Galagan.dm@gmail.com>
2020-09-20 15:25:46 +0300

    add completed PHOTOS.js

--- server/PHOTOS.js
+++ server/PHOTOS.js
@@ -5,34 +5,22 @@ const moment = require('moment'); // require
 const mv = require('mv');
 (async () => {
     const reader = await fs.readdirSync(path, 'utf-8');
-    let photos;
+    console.log(reader.length);
+    //let photos;
     for (let i = 0; i < reader.length; i++) {
-
-        const pathToNewdDir = path + '\\sorted_photos'; //'\\IMG-cb3c608518029aa1360bc5889d6ccbcc-V.jpg';
-
-           const pathToPhoto = path + `${reader[i]}`// путь к фото
-
-        const meta = await fileMetadata.getFileProperties(path + '\\20200121_115228.jpg');// доп. инфа по фото
-
+        const photo = reader[i];
+        //  console.log(reader[i]);
+        const pathToDir = path + '\\sorted_photos';
+        const meta = await fileMetadata.getFileProperties(path + `\\${photo}`);// доп. инфа по фото
+        //  const nameOfFile = meta.Name.split('\\')[5];// имя файла
         const creationDate = await moment(meta.CreationDate.split('.')[0], 'YYYYMMDDhhmmss');//дата создания
-
         const monthOfCreation = await creationDate.format('MMMM');//месяц создания
-
-        console.log(monthOfCreation);
-
-        const nameOfFile = meta.Name.split('\\')[5];// имя файла
-
-        console.log(nameOfFile)
-
         const yearOfCreation = await creationDate.format('YYYY');// год создания
-
-        console.log(yearOfCreation)
-
-        mv(path + '\\' + photos, `${pathToNewdDir}/${yearOfCreation}/${monthOfCreation}/${nameOfFile}`, {mkdirp: true}, (err) => {
-            if (err)
-                console.log(err)
-        });
+        //  console.log(i)
+        mv(path + '\\' + `${photo}`, `${pathToDir}/${yearOfCreation}/${monthOfCreation}/${photo}`, {mkdirp: true}, (err) => {
+            if (err) {
+                console.error(err);
+            }
+        })
     }
-
-
 })();
